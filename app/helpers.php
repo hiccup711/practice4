b4ec8e028edf2eaf97195f40c6cec952941f9486
Ricky <hiccup711@gmail.com>
2020-05-15 13:38:24 +0000

    更新数据库配置

--- app/helpers.php
+++ app/helpers.php
@@ -10,14 +10,14 @@
                 'database' => substr($url['path'],1),
                 'username' => $url['user'],
                 'password' => $url['pass']
-            ];
+        ];
         }else{
             return $db_config = [
                 'connection' => env('DB_CONNECTION', 'mysql'),
                 'host' => env('DB_HOST', 'localhost'),
                 'database' => env('DB_DATABASE', 'forge'),
                 'username' => env('DB_USERNAME', 'forge'),
-                'password' => env('DB_PASSWORD', 'forge')
-            ];
-        }
+                'password' => env('DB_PASSWORD', '')
+        ];
+    }
     }

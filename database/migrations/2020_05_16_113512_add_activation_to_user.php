d0708a414be09b2e4c6f8b87d40b6c06936e7b60
Ricky <hiccup711@gmail.com>
2020-05-16 05:00:37 +0000

    完成用户Curd

--- database/migrations/2020_05_16_113512_add_activation_to_user.php
+++ database/migrations/2020_05_16_113512_add_activation_to_user.php
@@ -14,7 +14,7 @@ class AddActivationToUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin');
+            $table->boolean('is_admin')->default(false);
             $table->string('activation_token')->nullable()->index();
             $table->boolean('activated')->default(false);
         });
